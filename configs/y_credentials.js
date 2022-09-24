90ea261fe855a82dc8c7f1d426bb0eacc0d73d77
Aliaksandr Danilin <alex39292@mail.ru>
2022-09-24 22:36:06 +0300

    add configs

---
+++ configs/y_credentials.js
@@ -0,0 +1,7 @@
+module.exports = {
+    ID: process.env.yandex_id,
+    PASSWORD: process.env.yandex_password,
+    CALLBACK_URL: process.env.yandex_callback_url,
+    TOKEN: process.env.yandex_token,
+    API_KEY: process.env.yandex_api_key
+}
