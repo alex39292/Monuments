38ffa847f2434f8da394d1864cb704190ac02712
alex39292 <50744099+alex39292@users.noreply.github.com>
2022-09-26 22:02:09 +0300

    add main code to the app

--- app.js
+++ app.js
@@ -8,48 +8,48 @@ pg.turnConnection();
 app.get('/', (req, res) => {
   res.send('helloe');
 })
-//app.route('/')
-//  .get((req, res) => {
-//    session = req.session;
-//    if (session.user) {
-//      res.render('admin');
-//    } else {
-//      res.render('login', {
-//        locals: {
-//          errorMessage: ''
-//        }
-//      });
-//    }
-//  })
-//  .post(async (req, res) => {
-//    if (req.session.user) {
-//      if (req.body.create) {
-//        res.render('create');
-//      }
-//      if (req.body.view) {
-//        await renderHelper.createView(req, res);
-//      }
-//      if (req.body.name && req.files) {
-//        await renderHelper.createUser(req, res);
-//      }
-//    } else if (req.body.password) {
-//      if (req.body.password === password) {
-//        session = req.session;
-//        session.user = req.body.password;
-//        res.render('admin');
-//      } else {
-//        res.render('login', {
-//        locals: {
-//          errorMessage: 'Неправильный пароль!'
-//        }});
-//      }
-//    } else {
-//      res.render('login', {
-//        locals: {
-//          errorMessage: 'Пожалуйста зарегистрируйтесь!'
-//        }});
-//    }
-//});
+app.route('/')
+  .get((req, res) => {
+    session = req.session;
+  if (session.user) {
+      res.render('admin');
+    } else {
+      res.render('login', {
+        locals: {
+          errorMessage: ''
+        }
+      });
+    }
+  })
+  .post(async (req, res) => {
+    if (req.session.user) {
+     if (req.body.create) {
+        res.render('create');
+      }
+      if (req.body.view) {
+        await renderHelper.createView(req, res);
+      }
+      if (req.body.name && req.files) {
+        await renderHelper.createUser(req, res);
+     }
+    } else if (req.body.password) {
+      if (req.body.password === password) {
+        session = req.session;
+        session.user = req.body.password;
+        res.render('admin');
+      } else {
+        res.render('login', {
+        locals: {
+          errorMessage: 'Неправильный пароль!'
+        }});
+      }
+    } else {
+      res.render('login', {
+        locals: {
+         errorMessage: 'Пожалуйста зарегистрируйтесь!'
+       }});
+   }
+});
 
 app.route('/:userId(\\w+\)')
 .get(async (req, res) => {
